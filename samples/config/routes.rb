e89f90506545563ead69261eb6f88cb93b4e4622
Jussi Räsänen <jussi@skyred.fi>
2016-02-10 14:57:29 +0200

    Fixed rails routes syntax

--- samples/config/routes.rb
+++ samples/config/routes.rb
@@ -1,5 +1,5 @@
 AdaptivePaymentsSamples::Engine.routes.draw do
-  post  "adaptive_payments/ipn_notify" => "adaptive_payments#ipn_notify", :as => :ipn_notify
-  match "adaptive_payments(/:action)",  :controller => "adaptive_payments", :as => :adaptive_payments
+  post  "adaptive_payments/ipn_notify", to: 'adaptive_payments#ipn_notify', :as => :ipn_notify
+  get "adaptive_payments(/:action)",  to: 'adaptive_payments', :as => :adaptive_payments
   root :to => "adaptive_payments#index"
 end
